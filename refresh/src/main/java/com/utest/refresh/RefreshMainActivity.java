649816294210fb3524d06dda8ff73647c531ae3f
lowry <326464723@qq.com>
2016-08-19 14:33:17 +0800

    asdasdasd

---
+++ refresh/src/main/java/com/utest/refresh/RefreshMainActivity.java
@@ -0,0 +1,13 @@
+package com.utest.refresh;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RefreshMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_refresh_main);
+    }
+}
